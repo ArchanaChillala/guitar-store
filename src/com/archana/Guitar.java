1dbf288ea9310ddd87a28f2659ee49a0d3fb81af
ArchanaChillala <archanac@thoughtworks.com>
2018-02-05 23:15:36 +0530

    Principle: Unused properties are a dead giveaway

--- src/com/archana/Guitar.java
+++ src/com/archana/Guitar.java
@@ -1,23 +1,15 @@
 package com.archana;
 
 
-public class Guitar {
+public class Guitar extends GuitarSpec {
+
     private String serialNumber;
     private double price;
-    private Builder builder;
-    private String model;
-    private Type type;
-    private Wood backWood;
-    private Wood topWood;
 
     public Guitar(String serialNumber, double price, Builder builder, String model, Type type, Wood backWood, Wood topWood) {
+        super(builder, model, type, backWood, topWood);
         this.serialNumber = serialNumber;
         this.price = price;
-        this.builder = builder;
-        this.model = model;
-        this.type = type;
-        this.backWood = backWood;
-        this.topWood = topWood;
     }
 
     public String getSerialNumber() {
@@ -28,32 +20,12 @@ public class Guitar {
         return price;
     }
 
-    public Builder getBuilder() {
-        return builder;
-    }
-
-    public String getModel() {
-        return model;
-    }
-
-    public Type getType() {
-        return type;
-    }
-
-    public Wood getBackWood() {
-        return backWood;
-    }
-
-    public Wood getTopWood() {
-        return topWood;
-    }
-
     @Override
     public String toString() {
         return "Guitar {" +
             "serialNumber='" + serialNumber + '\'' +
-            ", builder='" + builder + '\'' +
-            ", model='" + model + '\'' +
+            ", builder='" + super.getBuilder() + '\'' +
+            ", model='" + super.getModel() + '\'' +
             '}';
     }
 }
